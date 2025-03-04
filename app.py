cdab1a7907ca5f75567d55d0d8fce1657d908441
Kartik5199 <143116126+Kartik5199@users.noreply.github.com>
2025-03-04 18:22:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -50,5 +50,5 @@ def predict():
 
     return jsonify({'legitimacy_percentage': legitimacy_percentage, 'status': safety_status})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
